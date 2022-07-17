12681cd4bcbb33f8116fffea2666a284ee9b0551
Jnqa <ligez@yandex.ru>
2022-07-17 05:16:59 +0300

    🎨 palete and pipeine fix

--- jnqa-fun/src/App.js
+++ jnqa-fun/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import packageJson from '../package.json';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Jnqa's App.</code>
+          <code>Jnqa's App. {packageJson.version}</code>
         </p>
       </header>
     </div>
